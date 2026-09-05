808bc722916936d00f023f1de9458d76f0d6b400
Vinicius Collaço <viniciuscollaco@hotmail.com>
2026-09-05 11:50:59 -0300

    liçao primeira semana

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Olá, Mundo! Este é o Projeto Sandbox.");
+        Console.WriteLine("Isso é em C#");
+
+        int numero = 5;
+        numero = 9;
+        numero = numero + 5;
+
+        string cor = "azul";
+
+        if (numero > 3)
+        {
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine("Qual é a sua cor favorita ? ");
+        string corFav = Console.ReadLine();
+        Console.WriteLine($"Sua cor favorita é {corFav}");
     }
 }
