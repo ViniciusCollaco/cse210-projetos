e963124b17f4c71eb5295f0c7657301844abb4e5
Vinicius Collaço <viniciuscollaco@hotmail.com>
2026-09-05 14:16:49 -0300

    exercio 2 da semana 1

--- semana01/Exercicio1/Program.cs
+++ semana01/Exercicio1/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Qual é o seu nome? ");
-        string nome = Console.ReadLine();
+        Console.Write("Qual é o seu primeiro nome? ");
+        string nomePrimeiro = Console.ReadLine();
         Console.Write("Qual é o seu sobrenome? ");
         string sobrenome = Console.ReadLine();
 
-        Console.WriteLine($"Seu nome é {sobrenome}, {nome} {sobrenome}");
+        Console.WriteLine($"Seu nome é {sobrenome}, {nomePrimeiro} {sobrenome}");
     }
 }
--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Qual foi sua nota? ");
+        string notaString = Console.ReadLine();
+        int nota = int.Parse(notaString);
+
+        string letra = "";
+
+        if (nota >= 90)
+        {
+            letra = "A";
+        }
+        else if (nota >= 80)
+        {
+            letra = "B";
+        }
+        else if (nota >= 70)
+        {
+            letra = "C";
+        }
+        else if (nota >= 60)
+        {
+            letra = "D";
+        }
+        else
+        {
+            letra = "F";
+        }
+
+        string sinal = "";
+        int ultimoDigito = nota % 10;
+
+        if (ultimoDigito >= 7)
+        {
+            sinal = "+";
+        }
+        else if (ultimoDigito < 3)
+        {
+            sinal = "-";
+        }
+
+        if (letra == "A" && sinal == "+")
+        {
+            sinal = "";
+        }
+        else if (letra == "F")
+        {
+            sinal = "";
+        }
+
+        Console.WriteLine($"Sua nota é: {letra}{sinal}");
+
+        if (nota >= 70)
+        {
+            Console.WriteLine("Parabéns! Você foi aprovado no curso!");
+        }
+        else
+        {
+            Console.WriteLine("Não desanime! Continue se esforçando para a próxima vez.");
+        }
     }
 }
