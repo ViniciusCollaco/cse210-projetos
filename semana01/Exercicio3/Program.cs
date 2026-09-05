b3c309b5b3a4b8890df4ce3ffb1c888ee30b3aee
Vinicius Collaço <viniciuscollaco@hotmail.com>
2026-09-05 15:40:26 -0300

    3 exercicio

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        Random geradorAleatorio = new Random();
+        string jogarDeNovo = "sim";
+
+        while (jogarDeNovo.ToLower() == "sim")
+        {
+            int numeroMagico = geradorAleatorio.Next(1, 101);
+            int resposta = -1;
+            int contadorPalpites = 0; 
+
+            while (resposta != numeroMagico)
+            {
+                Console.Write("Qual é o seu palpite? ");
+                string respostaString = Console.ReadLine();
+                resposta = int.Parse(respostaString);
+                
+                contadorPalpites++;
+
+                if (resposta < numeroMagico)
+                {
+                    Console.WriteLine("Mais alto");
+                }
+                else if (resposta > numeroMagico)
+                {
+                    Console.WriteLine("Mais baixo");
+                }
+                else
+                {
+                    Console.WriteLine("Você adivinhou!");
+                }
+            }
+
+            Console.WriteLine($"Você precisou de {contadorPalpites} palpites para acertar!");
+
+            Console.Write("Deseja jogar de novo (sim/não)? ");
+            jogarDeNovo = Console.ReadLine();
+            Console.WriteLine(); 
+        }
+
+        Console.WriteLine("Obrigado por jogar!");
     }
 }
